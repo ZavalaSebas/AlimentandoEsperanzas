c57639f127ef00d3152aa8b84310e48e4fee4056
Sebas Zavala <sebaszavala120@gmail.com>
2024-02-07 21:13:46 -0600

    Update Reformateo
    -Tablas Reformateo

--- AlimentandoEsperanzas/obj/Debug/net8.0/AlimentandoEsperanzas.AssemblyInfo.cs
+++ AlimentandoEsperanzas/obj/Debug/net8.0/AlimentandoEsperanzas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dceab9b0582acfbe9bd00a050b3ec82b005a08d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ffd615bd5344f8179cae2109376281f4f37b56")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
