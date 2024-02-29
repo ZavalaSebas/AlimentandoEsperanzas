a845b6a7e4c1fd5b0cddfd36a64524e83ee24e5a
Sebastian <105946105+SebasBalles1999@users.noreply.github.com>
2024-02-28 21:00:07 -0600

    Login y preterminado

---
+++ AlimentandoEsperanzas/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlimentandoEsperanzas.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string usuario)
+        {
+            ViewBag.usuario = usuario;
+            return View();
+        }
+    }
+}
--- AlimentandoEsperanzas/obj/Debug/net8.0/AlimentandoEsperanzas.AssemblyInfo.cs
+++ AlimentandoEsperanzas/obj/Debug/net8.0/AlimentandoEsperanzas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0b52e6a0a487e8d615890debb3e65625d5bf6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbbf7be722f71a3e28ba978206859c9f71c9e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
