15d2ff7ffc80bbe2e6730c3e2a9ba42fdc72b358
Sebas Zavala <sebaszavala120@gmail.com>
2024-03-03 12:13:26 -0600

    FIX Version

--- AlimentandoEsperanzas/obj/Debug/net8.0/AlimentandoEsperanzas.AssemblyInfo.cs
+++ AlimentandoEsperanzas/obj/Debug/net8.0/AlimentandoEsperanzas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0b52e6a0a487e8d615890debb3e65625d5bf6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e36eb14d0adf37bb3757dd06e2912e1238cb8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlimentandoEsperanzas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
